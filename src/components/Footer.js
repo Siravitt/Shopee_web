292351039b876148284634ae7392a3462a4487dd
kisada123 <kisada.le55@sau.ac.th>
2023-02-20 12:47:20 +0700

    merge product component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,25 @@
-import home from "../images/home.png";
-import chat from "../images/chat.png";
-import warn from "../images/warn.png";
-import profile from "../images/profile.png";
+// import home from "../images/home.png";
+// import chat from "../images/chat.png";
+// import warn from "../images/warn.png";
+// import profile from "../images/profile.png";
 
-export default function Footer() {
-  return (
-    <>
-      <div className="w-[390px] h-[70px] px-8 bg-orange-400 flex items-center justify-between  bottom-0 fixed">
-        <div className="w-[38px] h-[38px] rounded-full flex items-center justify-center">
-          <img src={home} alt="" />
-        </div>
-        <div className="w-[38px] h-[38px] rounded-full flex items-center justify-center">
-          <img src={chat} alt="" />
-        </div>
-        <div className="w-[38px] h-[38px] rounded-full flex items-center justify-center">
-          <img src={warn} alt="" />
-        </div>
-        <div className="w-[38px] h-[38px] rounded-full flex items-center justify-center">
-          <img src={profile} alt="" />
-        </div>
-      </div>
-    </>
-  );
-}
+// export default function Footer() {
+//   return (
+//     <>
+//       <div className="w-[390px] h-[70px] px-8 bg-orange-400 flex items-center justify-between  bottom-0 fixed">
+//         <div className="w-[38px] h-[38px] rounded-full flex items-center justify-center">
+//           <img src={home} alt="" />
+//         </div>
+//         <div className="w-[38px] h-[38px] rounded-full flex items-center justify-center">
+//           <img src={chat} alt="" />
+//         </div>
+//         <div className="w-[38px] h-[38px] rounded-full flex items-center justify-center">
+//           <img src={warn} alt="" />
+//         </div>
+//         <div className="w-[38px] h-[38px] rounded-full flex items-center justify-center">
+//           <img src={profile} alt="" />
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
