5293243565b5b19e03cdc36165f44d262f539b61
colorkram <colorkram@gmail.com>
2023-03-02 22:14:39 +0700

    bigBug2

--- src/components/CardShop.js
+++ src/components/CardShop.js
@@ -1,23 +1,19 @@
-// import {
-//   // thunkFetchAllProduct,
-//   thunkFetchAllProductByShopId,
-// } from "../reduxStore/ProductSlice";
+import { thunkFetchAllProductByShopId } from "../reduxStore/ProductSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
 export default function CardShop(props) {
   const dispatch = useDispatch();
 
-  const state = useSelector((state) => state.allProduct.productFilterShop);
+  const state = useSelector(state => state.allProduct.productFilterShop);
 
-  // useEffect(() => {
-  //   dispatch(thunkFetchAllProductByShopId(props.shopId));
-  // }, [dispatch]);
-
+  useEffect(() => {
+    dispatch(thunkFetchAllProductByShopId(props.shopId));
+  }, [dispatch]);
 
   return (
     <>
-      {state?.productShopId?.map((item) => {
+      {state?.productShopId?.map(item => {
         return (
           <div className="">
             <div
@@ -29,7 +25,7 @@ export default function CardShop(props) {
               // }}
             >
               <div className=" ">
-                {item.ProductImages.map((data) => {
+                {item.ProductImages.map(data => {
                   return (
                     <img
                       className="p-auto rounded-t-lg  "
