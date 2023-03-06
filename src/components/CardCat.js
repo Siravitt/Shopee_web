9ffcd052decf34538a85376e34a5a4f4c4ab719c
Momeksirinapapong <ms.supapornn@gmail.com>
2023-03-06 10:19:15 +0700

    AddNewAddressNewBranch

--- src/components/CardCat.js
+++ src/components/CardCat.js
@@ -4,6 +4,7 @@ import {
 } from "../reduxStore/ProductSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function CardCat(props) {
   const dispatch = useDispatch();
@@ -24,40 +25,41 @@ export default function CardCat(props) {
       {state?.map(item => {
         return (
           <div className="" key={item.id + "hello"}>
-            <div
-              className="w-auto h-auto bg-white border border-gray-200 rounded-lg shadow bg-white border-gray-700s "
+            <Link to={`/product/${item.id}`}>
+              <div
+                className="w-auto h-auto bg-white border border-gray-200 rounded-lg shadow bg-white border-gray-700s "
 
-              // onClick={() => {
+                // onClick={() => {
 
-              // }}
-            >
-              <div className=" ">
-                {item?.ProductImages?.map(data => {
-                  return (
+                // }}
+              >
+                <div className=" ">
+                  {
                     <img
                       className="p-auto rounded-t-lg  "
-                      src={data.image}
+                      src={item.ProductImages.find(i => !!i.isMain)?.image}
                       alt="product"
+                      key={item.id + "cardImage"}
                     />
-                  );
-                })}
-              </div>
+                  }
+                </div>
 
-              <div className="px-3 pb-1">
-                <h5 className="text-xl tracking-tight text-gray-900 flex justify-left pl-0 pt-2">
-                  {item.name}
-                </h5>
+                <div className="px-3 pb-1">
+                  <h5 className="text-xl tracking-tight text-gray-900 flex justify-left pl-0 pt-2">
+                    {item.name}
+                  </h5>
 
-                <div className="flex justify-between">
-                  <span className="text-[20px] mt-[8px] text-gray-900  text-red">
-                    100
-                  </span>
-                  <div className="text-black  font-medium rounded-lg text-sm mt-[16px]   text-center  ">
-                    ขายแล้ว 0 ชิ้น
+                  <div className="flex justify-between">
+                    <span className="text-[20px] mt-[8px] text-gray-900  text-red">
+                      100
+                    </span>
+                    <div className="text-black  font-medium rounded-lg text-sm mt-[16px]   text-center  ">
+                      ขายแล้ว 0 ชิ้น
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Link>
           </div>
         );
       })}
