47d17986aad82de243d70480935df65d18129ddf
colorkram <119280422+colorkram@users.noreply.github.com>
2023-03-11 17:21:49 +0700

    Update ProtectedShop.js

--- src/utils/ProtectedShop.js
+++ src/utils/ProtectedShop.js
@@ -2,9 +2,9 @@ import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedShop({ children }) {
-  const auth = useSelector((state) => state.auth.auth);
-  if (auth.is_shop) {
-    return children;
+  const auth = useSelector(state => state.auth.auth);
+  if (!auth || auth.is_shop === false) {
+    return <Navigate to="/" />;
   }
-  return <Navigate to="/" />;
+  return children;
 }
